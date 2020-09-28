202159177e229d228ab5913b2dff085de10e3120
FortisTbo <mail4tbo@gmail.com>
2020-09-28 15:00:19 +0200

    ExA6

--- ExA6/src/be/abis/exa6/test/ExA6.java
+++ ExA6/src/be/abis/exa6/test/ExA6.java
@@ -1,6 +1,11 @@
 package be.abis.exa6.test;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import be.abis.exa6.model.Address;
 import be.abis.exa6.model.Company;
@@ -20,7 +25,32 @@ public class ExA6 {
 		
 		System.out.println(person2);
 
-
+		ArrayList <Person> persons = new ArrayList <> ();
+		persons.add(person);
+		persons.add(person2);
+		
+		try {
+			printPersony (persons);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printPersony(ArrayList <Person> persons) throws IOException {
+		
+		BufferedWriter bw = Files.newBufferedWriter(Paths.get("personinfo.txt"));
+		
+		String nextStr = ", ";
+		for (int i = 0 ; i < persons.size(); i++) {
+			Person person =  persons.get(i);
+			bw.write(person + nextStr);
+			if  (i ==  persons.size()-2) {
+				nextStr = "\n";
+			}
+		}
+		bw.close();
+		
 	}
 
 }
